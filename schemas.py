c1fb341b84215afbfe61d42dda8a4a2090865f41
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-19 22:24:16 +0300

    Add Pydantic schemas for a blog app

---
+++ schemas.py
@@ -0,0 +1,38 @@
+
+# These schemas define the structure of data accepted and returned by the blog app.
+
+from pydantic import BaseModel
+
+# Define Pydantic schema for creating a new blog post
+class BlogPostCreate(BaseModel):
+    title: str
+    category: str
+    subCategory: str
+    content: str
+    authorName: str
+    authorAvatar: str
+    cover: str
+
+# Define Pydantic schema for the response when creating or retrieving a blog post
+class BlogPostResponse(BlogPostCreate):
+    id: int
+    createdAt: str
+
+# Define Pydantic schema for user input data
+class UserSchema(BaseModel):
+    name: str
+    phone: str
+
+# Define Pydantic schema for comment input data
+class CommentSchema(BaseModel):
+    comment_text: str
+
+# Define Pydantic schema for the response when creating or retrieving a comment
+class CommentResponse(CommentSchema):
+    id: int
+
+# Define Pydantic schema for the response when creating or retrieving a user
+class UserResponse(UserSchema):
+    id: int
+
+
