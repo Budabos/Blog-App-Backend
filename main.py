dfa8a120936565e67eb142d72ab707a89219a117
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-19 17:41:35 +0300

    updates

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+# Importing BaseModel from Pydantic
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    id: int
+    title: str
+    category: str
+    subCategory: str
+    description: str
+    authorName: str
+    authorAvatar: str
+    createdAt: datetime
+    cover: str
+    
+        
+    
 
 app = FastAPI()
 
-# Define a route
+# Define a routes
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
